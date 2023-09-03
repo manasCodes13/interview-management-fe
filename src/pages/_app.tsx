351802e648bcdca4aad0070c7e8373bc059c7943
manasCodes13 <amr1313mk@gmail.com>
2023-09-03 23:54:57 +0530

    added layout in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,33 @@ import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import Layout from "@/components/Layout/Layout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const urlsWithoutLayout = [
+    "/auth/login",
+    "/auth/register",
+    "/auth/organization",
+    "/auth/organization/createOrganization",
+  ];
+
   return (
     <>
-    <Layout >
-      <Component {...pageProps} />
-      <ToastContainer />
-    </Layout>
+      {urlsWithoutLayout?.includes(router?.pathname) ? (
+        <>
+          <Component {...pageProps} />
+          <ToastContainer />
+        </>
+      ) : (
+        <>
+          <Layout>
+            <Component {...pageProps} />
+            <ToastContainer />
+          </Layout>
+        </>
+      )}
     </>
   );
 }
